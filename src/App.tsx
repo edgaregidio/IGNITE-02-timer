1fb51559170f1a574fbbe6f124b2dc928f8e710a
edgaregidio <edgarsiqueirasi@gmail.com>
2023-04-09 19:45:13 -0300

    exemplo de context react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
-import { GlobalStyle } from './styles/global'
+// import { ThemeProvider } from 'styled-components'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Router } from './Router'
+// import { GlobalStyle } from './styles/global'
 
-import { defaultTheme } from './styles/themes/default'
+// import { defaultTheme } from './styles/themes/default'
+import { Home } from './Home'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={defaultTheme}>
-        <Router />
-        <GlobalStyle />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Home />
   )
+  // return (
+  //   <BrowserRouter>
+  //     <ThemeProvider theme={defaultTheme}>
+  //       <Router />
+  //       <GlobalStyle />
+  //     </ThemeProvider>
+  //   </BrowserRouter>
+  // )
 }
